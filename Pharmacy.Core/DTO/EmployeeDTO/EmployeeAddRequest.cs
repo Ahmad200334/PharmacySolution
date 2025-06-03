9e5c7dfde5ac50ced2aab1f014ff1904eece2979
Ahmad200334 <eng.ahmadkhlaed21@gmail.com>
2025-06-03 23:38:38 +0300

    شرح التغييرات اللي عملتها

---
+++ Pharmacy.Core/DTO/EmployeeDTO/EmployeeAddRequest.cs
@@ -0,0 +1,47 @@
+﻿ 
+    using Pharmacy.Core.Enum;
+using Pharmacy.Core.Domain.Entities.IdentityEntities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pharmacy.Core.DTO.EmployeeDTO
+{
+   public class EmployeeAddRequest
+    {
+        [Required]
+        public string UserName { set; get; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { set; get; }
+
+        [RegularExpression("^(\\d{1,15})$")]
+        public string? phone { set; get; }
+
+        public DateTime? HireDate { set; get; } = DateTime.Now;
+        
+        
+        public float? Salary{ set; get; }
+
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "كلمة السر يجب أن تكون أكبر من 8 أحرف")]
+        [Required]
+        public string  Password { set; get; }
+
+        public ApplicationUser? User { set; get; }
+
+        public Guid UesrId { set; get; }
+
+         
+
+        public Employee ToEmployee()
+        {
+            return new Employee()
+            {
+                Salary = this.Salary,
+                HireDate = this.HireDate,
+                User=User,
+                UserId=this.UesrId
+               
+            };
+        }
+    }
+}
